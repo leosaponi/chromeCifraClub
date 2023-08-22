36d129c8977ffcbda42351070df3fce5fe23750a
leosaponi <70208412+leosaponi@users.noreply.github.com>
2023-08-22 11:28:42 -0300

    Update background.js

--- background.js
+++ background.js
@@ -5,16 +5,13 @@ chrome.contextMenus.create({
     contexts: ["selection"]
   });
   
-  // Adiciona um listener para o evento de clique na entrada do menu de contexto
   chrome.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId === "searchOnCifraClub") {
       // Obtém o texto selecionado
       var selectionText = info.selectionText;
   
-      // Cria uma nova URL com o texto selecionado
       var searchUrl = "https://www.cifraclub.com.br/?q=" + encodeURIComponent(selectionText);
   
-      // Abre a URL em uma nova aba
       chrome.tabs.create({ url: searchUrl });
     }
   });
