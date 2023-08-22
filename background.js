4bb033c0715e9b52eade94d9687f0b2ac56b4bd6
Leoanrdo Saponi <sleo028@gmail.com>
2023-08-22 11:41:38 -0300

    Feat: Changing manifest version

--- background.js
+++ background.js
@@ -1,18 +1,14 @@
-// Cria uma entrada no menu de contexto
+
 chrome.contextMenus.create({
-    id: "searchOnCifraClub",
-    title: "Buscar no Cifra Club: %s", // O %s indica onde o texto selecionado deve ser inserido
-    contexts: ["selection"]
-  });
-  
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "searchOnCifraClub") {
-      // Obtém o texto selecionado
-      var selectionText = info.selectionText;
-  
-      var searchUrl = "https://www.cifraclub.com.br/?q=" + encodeURIComponent(selectionText);
-  
-      chrome.tabs.create({ url: searchUrl });
-    }
-  });
-  
+  id: "searchOnCifraClub",
+  title: "Buscar no Cifra Club: %s",
+  contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "searchOnCifraClub") {
+    var selectionText = info.selectionText;
+    var searchUrl = "https://www.cifraclub.com.br/?q=" + encodeURIComponent(selectionText);
+    chrome.tabs.create({ url: searchUrl });
+  }
+});
